df39278c42ec780d6f4ad8e8c5bfef2a2281ff8b
Begzod Bakhriddinov <beka083bakhriddinov@gmail.com>
2021-02-21 20:34:47 +0500

    Initial Commit

---
+++ CodeExampleC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CodeExampleC
+//
+//  Created by Begzod Bakhriddinov on 21/02/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
