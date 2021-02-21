9542a2d8acfee599b0d4a21058a9e82446bb9f72
Begzod Bakhriddinov <beka083bakhriddinov@gmail.com>
2021-02-21 23:07:10 +0500

    variables and simple operations

--- CodeExampleC/main.c
+++ CodeExampleC/main.c
@@ -6,9 +6,41 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main() {
+    
+//    // INTegers
+//    int x = 4, y = 12, z;
+//    x *= 3;
+//    x++;
+//
+//    z = x + y;
+//
+//    // FLOATing numbers
+//    float a, b, c;
+//    a = 9.1f;
+//    b = 7.3f;
+//    c = 1.4f;
+//
+//    double d = 432.27509f;
+//
+//    // CHARacters
+//    char dollarSign = '$';
+//
+//    // BOOLeans
+//    bool isCoder = false;
+//
+//    printf("Variable: %d + %d = %d\n", x, y, z);
+    
+    // float x, y, res;
+    
+    // scanf("%f", &x);
+    // scanf("%f", &y);
+    
+    // res = x * y;
+    
+    // printf("Result: %.2f\n", res);
+    
+    return 0 ;
 }
