789f2b4f96e18e074845dad575ee3ffa3e3368a0
Begzod Bakhriddinov <beka083bakhriddinov@gmail.com>
2021-02-22 20:19:30 +0500

    arrays

--- CodeExampleC/main.c
+++ CodeExampleC/main.c
@@ -10,14 +10,14 @@
 
 int main() {
     
-//    // INTegers
+//    // INTEGERS
 //    int x = 4, y = 12, z;
 //    x *= 3;
 //    x++;
 //
 //    z = x + y;
 //
-//    // FLOATing numbers
+//    // FLOATING NUMBERS
 //    float a, b, c;
 //    a = 9.1f;
 //    b = 7.3f;
@@ -25,10 +25,10 @@ int main() {
 //
 //    double d = 432.27509f;
 //
-//    // CHARacters
+//    // CHARACTERS
 //    char dollarSign = '$';
 //
-//    // BOOLeans
+//    // BOOLEANS
 //    bool isCoder = false;
 //
 //    printf("Variable: %d + %d = %d\n", x, y, z);
@@ -41,6 +41,20 @@ int main() {
     // res = x * y;
     
     // printf("Result: %.2f\n", res);
+    
+    // ARRAYS
+    int array1[] = {1, 8, 3, 9123, 34};
+    printf("%d\n", array1[3]); // prints "9123"
+    
+    float array2[] = {3.2f, 902.5f, 1.9f, 54.901f};
+    printf("%.2f\n", array2[2]); // prints "1.90"
+    
+    char array3[] = {'w', 'o', 'r', 'd'};
+    printf("%c\n", array3[0]); // prints "w"
+    
+    char array4[] = "Hello Swift!";
+    printf("%c\n", array4[6]); // prints "S"
+    printf("%s\n", array4);
     
     return 0 ;
 }
